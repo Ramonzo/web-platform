b695d0bb081fa4ecd1eff9c6ed2cd406226dfbb4
Ramon Lago Alves Freire <ramonlago@outlook.com>
2020-06-14 18:13:13 -0300

    preparando terreno da aplicação

---
+++ platform/functions.js
@@ -0,0 +1,4 @@
+//Faz recalculos caso a tela seja alterada de tamanho.
+function windowResized() {
+  resizeCanvas(screenW, screenH);
+}
--- platform/platform.js
+++ platform/platform.js
@@ -1,8 +1,19 @@
-function setup() {
+let screenW = 0;
+let screenH = 0;
+let mapImg;
 
+function preload(){
+  mapImg = loadImage("assets/img/map.png");
 }
 
+function setup() {
+  screenW = (windowWidth*61)/100;
+  screenH = (windowHeight*80)/100;
+  let myCanvas = createCanvas(screenW, screenH);
+  myCanvas.parent("map");
+}
 
 function draw() {
-
+  background(255);
+  image(mapImg, -3000, -3000, 5000, 5000);
 }
