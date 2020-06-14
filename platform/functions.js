d2af27eeef51580bceb419d9a72c332c81ad1fb0
Ramon Lago Alves Freire <ramonlago@outlook.com>
2020-06-14 18:34:46 -0300

    criando arquivos e fazendo nodes das rodas da rodovia

--- platform/functions.js
+++ platform/functions.js
@@ -2,8 +2,29 @@
 function windowResized() {
   resizeCanvas(screenW, screenH);
 }
+//Draw highway lines
 function drawLines(){
+  //Line Proportie
   stroke(color("#D3F4E1"));
-  strokeWeight(4);
-  line(50,50,100,100);
+  strokeWeight(8);
+  //Lines
+  line(node[0][0], node[0][1], node[1][0], node[1][1]);
+  line(node[1][0], node[1][1], node[2][0], node[2][1]);
+  line(node[2][0], node[2][1], node[3][0], node[3][1]);
+  line(node[3][0], node[3][1], node[4][0], node[4][1]);
+  line(node[4][0], node[4][1], node[5][0], node[5][1]);
+  line(node[5][0], node[5][1], node[6][0], node[6][1]);
+  line(node[6][0], node[6][1], node[7][0], node[7][1]);
+  line(node[7][0], node[7][1], node[8][0], node[8][1]);
+  line(node[8][0], node[8][1], node[9][0], node[9][1]);
+  line(node[9][0], node[9][1], node[10][0], node[10][1]);
+}
+//Create echo toten representation
+function createToten(){
+}
+//Draw echo toten representation
+function drawToten(){
+  ellipseMode(CENTER);
+  noStronke();
+  
 }
--- platform/platform.js
+++ platform/platform.js
@@ -1,6 +1,25 @@
+//Screen Proportion based on parent div
 let screenW = 0;
 let screenH = 0;
+//Image Brazil
 let mapImg;
+//Node
+let node = [
+            [50, 50],
+            [90, 120],
+            [160, 180],
+            [210, 250],
+            [260, 300],
+            [300, 320],
+            [330, 350],
+            [400, 400],
+            [450, 430],
+            [480, 480]
+           ];
+//Toten Class
+let totens = [];
+//Driver Class
+let drivers = [];
 
 function preload(){
   mapImg = loadImage("assets/img/map.png");
@@ -11,10 +30,12 @@ function setup() {
   screenH = (windowHeight*80)/100;
   let myCanvas = createCanvas(screenW, screenH);
   myCanvas.parent("map");
+  createToten();
 }
 
 function draw() {
   background(255);
   image(mapImg, -3000, -3000, 5000, 5000);
   drawLines();
+  drawToten();
 }
